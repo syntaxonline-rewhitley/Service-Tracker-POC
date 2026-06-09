5d61caf037b4c7d9d1ad34a6078c514dde7df8e4
Roland Whitley <roland.whitley@syntaxonline.com>
2026-06-09 11:13:36 -0400

    added support for entity framework into application and added dependency injection

--- back-end/src/ServiceTracker.Api/Program.cs
+++ back-end/src/ServiceTracker.Api/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ServiceTracker.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ServiceTrackerDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
